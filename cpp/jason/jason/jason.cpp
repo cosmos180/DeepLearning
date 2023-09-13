a6ea808b1bc1a7e81efde606f004c34294dd9f6e
bughero <jinxinhou@tuputech.com>
2023-09-13 12:17:10 +0800

    cpp:feature:新增 epoll。

--- cpp/jason/jason/jason.cpp
+++ cpp/jason/jason/jason.cpp
@@ -1,16 +1,13 @@
 /*
  * @Author: bughero jinxinhou@tuputech.com
- * @Date: 2023-09-13 11:00:49
+ * @Date: 2023-09-13 12:02:14
  * @LastEditors: bughero jinxinhou@tuputech.com
- * @LastEditTime: 2023-09-13 12:04:55
- * @FilePath: /DeepLearning/cpp/jason/jason/jason.h
+ * @LastEditTime: 2023-09-13 12:05:08
+ * @FilePath: /DeepLearning/cpp/jason/jason/jason.cpp
  * @Description:
  *
  * Copyright (c) 2023 by 图普科技, All Rights Reserved.
  */
-#ifndef JASON_H
-#define JASON_H
-
 #include <cstdint>
 #include <stdio.h>
 
@@ -18,9 +15,10 @@ namespace JASON {
     namespace CPP {}
 
     namespace Linux {
-        int32_t epollTest();
+        int32_t __attribute__((weak)) epollTest() {
+            printf("[%s]: 1 - called.\n", __FUNCTION__);
+            return 0;
+        }
     } // namespace Linux
 
 } // namespace JASON
-
-#endif /* JASON_H */
