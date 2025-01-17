81e06914306db77da203ad172ca65d1ebfc512b4
HouJinxin <jinxinhou@tuputech.com>
2025-01-17 10:56:46 +0000

    feature:实现一个 rcurl。模仿 curl，可以交叉编译到不支持的平台，类似 sdc。

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,90 +1,55 @@
-/*
- * @Author       : HouJinxin jinxinhou@tuputech.com
- * @Date         : 2024-11-29 02:12:50
- * @LastEditors  : HouJinxin jinxinhou@tuputech.com
- * @LastEditTime : 2024-11-29 02:46:26
- * @FilePath     : /DeepLearning/rust/src/main.rs
- * @Description  :
- *
- * Copyright (c) 2024 by @Me, All Rights Reserved.
- */
+use reqwest::multipart;
+use structopt::StructOpt;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
 
-// use rusqlite::{Connection, Result};
+#[derive(StructOpt)]
+#[structopt(name = "rcurl", about = "A simple HTTP client.")]
+struct Opt {
+    #[structopt(short, long)]
+    upload: Option<String>,
 
-// #[derive(Debug)]
-// struct Person {
-//     id: i32,
-//     name: String,
-//     data: Option<Vec<u8>>,
-// }
+    #[structopt(short, long)]
+    download: Option<String>,
 
-// fn main() -> Result<()> {
-//     let conn = Connection::open_in_memory()?;
-
-//     conn.execute(
-//         "CREATE TABLE person (
-//             id    INTEGER PRIMARY KEY,
-//             name  TEXT NOT NULL,
-//             data  BLOB
-//         )",
-//         (), // empty list of parameters.
-//     )?;
-//     let me = Person {
-//         id: 0,
-//         name: "Steven".to_string(),
-//         data: None,
-//     };
-//     conn.execute(
-//         "INSERT INTO person (name, data) VALUES (?1, ?2)",
-//         (&me.name, &me.data),
-//     )?;
-
-//     let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-//     let person_iter = stmt.query_map([], |row| {
-//         Ok(Person {
-//             id: row.get(0)?,
-//             name: row.get(1)?,
-//             data: row.get(2)?,
-//         })
-//     })?;
-
-//     for person in person_iter {
-//         println!("Found person {:?}", person.unwrap());
-//     }
-
-//     Ok(())
-// }
-
-// use reqwest::Error;
-
-// #[tokio::main]
-// async fn main() -> Result<(), Error> {
-//     let response = reqwest::get("http://httpbin.org/get").await?;
-//     let body = response.text().await?;
-//     println!("body = {:?}", body);
-//     Ok(())
-// }
-
-use std::io;
-
-fn println_test() {
-    let x = 5;
-    let y = 10;
-
-    println!("x = {x} and y + 2 = {}", y + 2);
+    #[structopt(short, long)]
+    url: String,
 }
 
-fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
+async fn upload_file(url: &str, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let file = tokio::fs::read(file_path).await?;
+    let part = multipart::Part::bytes(file).file_name(file_path.to_string());
 
-    let mut guess = String::new();
+    let form = multipart::Form::new().part("file", part);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let client = reqwest::Client::new();
+    let res = client.post(url).multipart(form).send().await?;
 
-    println!("You guessed: {}", guess);
+    println!("Response: {:?}", res.text().await?);
+    Ok(())
+}
 
-    println_test();
+async fn download_file(url: &str, file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let res = client.get(url).send().await?;
+
+    let mut file = File::create(file_path).await?;
+    let mut content = res.bytes().await?;
+    tokio::io::copy(&mut content.as_ref(), &mut file).await?;
+
+    println!("File downloaded to {}", file_path);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opt = Opt::from_args();
+
+    if let Some(file_path) = opt.upload {
+        upload_file(&opt.url, &file_path).await?;
+    } else if let Some(file_path) = opt.download {
+        download_file(&opt.url, &file_path).await?;
+    }
+
+    Ok(())
 }
