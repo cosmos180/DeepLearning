9865abe75c321ca2369eac016f93f113bd1e4c72
HouJinxin <jinxinhou@tuputech.com>
2025-04-02 03:19:40 +0000

    feature:rust 增加 practise，方便快速测试。

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,19 @@
+/*
+ * @Author       : HouJinxin jinxinhou@tuputech.com
+ * @Date         : 2024-11-29 02:54:16
+ * @LastEditors  : HouJinxin jinxinhou@tuputech.com
+ * @LastEditTime : 2025-04-02 03:15:50
+ * @FilePath     : /DeepLearning/rust/src/main.rs
+ * @Description  :
+ *
+ * Copyright (c) 2025 by @Me, All Rights Reserved.
+ */
 use reqwest::multipart;
 use structopt::StructOpt;
 use tokio::fs::File;
-use tokio::io::AsyncReadExt;
 
 #[derive(StructOpt)]
-#[structopt(name = "rcurl", about = "A simple HTTP client.")]
+#[structopt(name = "rurl", about = "A rust http client that mimics curl.")]
 struct Opt {
     #[structopt(short, long)]
     upload: Option<String>,
@@ -34,22 +43,30 @@ async fn download_file(url: &str, file_path: &str) -> Result<(), Box<dyn std::er
     let res = client.get(url).send().await?;
 
     let mut file = File::create(file_path).await?;
-    let mut content = res.bytes().await?;
+    let content = res.bytes().await?;
     tokio::io::copy(&mut content.as_ref(), &mut file).await?;
 
     println!("File downloaded to {}", file_path);
     Ok(())
 }
 
+mod practise {
+    pub mod r_box;
+    pub mod r_list;
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let opt = Opt::from_args();
+    // let opt = Opt::from_args();
 
-    if let Some(file_path) = opt.upload {
-        upload_file(&opt.url, &file_path).await?;
-    } else if let Some(file_path) = opt.download {
-        download_file(&opt.url, &file_path).await?;
-    }
+    // if let Some(file_path) = opt.upload {
+    //     upload_file(&opt.url, &file_path).await?;
+    // } else if let Some(file_path) = opt.download {
+    //     download_file(&opt.url, &file_path).await?;
+    // }
+
+    practise::r_box::run();
+    practise::r_list::run();
 
     Ok(())
 }
